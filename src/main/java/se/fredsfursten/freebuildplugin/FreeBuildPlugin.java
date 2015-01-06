5317e6a9f920761d499327fb7c7c84b00faf02f0
vanbrayne <lars.lindberg@xlent.se>
2015-01-06 14:29:58 +0100

    Now uses PluginTools

--- src/main/java/se/fredsfursten/freebuildplugin/FreeBuildPlugin.java
+++ src/main/java/se/fredsfursten/freebuildplugin/FreeBuildPlugin.java
@@ -12,16 +12,18 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import se.fredsfursten.plugintools.PlayerInfo;
+
 public final class FreeBuildPlugin extends JavaPlugin implements Listener {
 	
-	private FreeBuilders freeBuilders;
+	private PlayerInfo<FreeBuilderInfo> freeBuilders;
 
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);		
 		ProtectFreeBuilders.get().enable();
 		Commands.get().enable(this);
-		this.freeBuilders = FreeBuilders.get();
+		this.freeBuilders = new PlayerInfo<FreeBuilderInfo>();
 	}
 
 	@Override
@@ -41,7 +43,7 @@ public final class FreeBuildPlugin extends JavaPlugin implements Listener {
 		
 		if (!(event.getEntity() instanceof Monster)) return;
 		
-		if (!this.freeBuilders.isFreeBuilder(player)) return;
+		if (!this.freeBuilders.hasInformation(player)) return;
 
 		event.setCancelled(true);
 	}
@@ -57,7 +59,7 @@ public final class FreeBuildPlugin extends JavaPlugin implements Listener {
 		if (!(event.getEntity() instanceof Monster)) return;
 		Monster monster = (Monster) event.getEntity();
 		
-		if (!this.freeBuilders.isFreeBuilder(player)) return;
+		if (!this.freeBuilders.hasInformation(player)) return;
 		
 		// You can attack monsters that targets you
 		if (monster.getTarget() == player) return;
@@ -74,7 +76,7 @@ public final class FreeBuildPlugin extends JavaPlugin implements Listener {
 		if (!(entity instanceof Player)) return;
 		Player player = (Player) entity;
 		
-		if (!this.freeBuilders.isFreeBuilder(player)) return;
+		if (!this.freeBuilders.hasInformation(player)) return;
 		
 		event.setCancelled(true);
 	}
